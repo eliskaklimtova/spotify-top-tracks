a7a7ebfec56d66679286f53760230b995197c201
eliskaklimtova <eli.klimtova@gmail.com>
2023-08-09 12:30:08 +0200

    Render track list

--- src/App.js
+++ src/App.js
@@ -1,43 +1,55 @@
 import "./App.css";
-import { useState } from "react";
-import { code, clientId, logIn, getAccessToken } from "./spotifyAPI";
+import { useState, useEffect } from "react";
+import {
+  code,
+  clientId,
+  logIn,
+  getAccessToken,
+  getTopTracks,
+} from "./spotifyAPI";
 
 function App() {
-  console.log('App render')
   const [token, setToken] = useState();
+  const [tracks, setTracks] = useState();
 
-  if (code) {
-    console.log("Code is set:", code)
-    const promise = getAccessToken(clientId, code)
-    promise.then((token) => setToken(token));
-  }
+  useEffect(() => {
+    if (code) {
+      const promise = getAccessToken(clientId, code);
+      promise.then((token) => setToken(token));
+    }
+  }, []);
 
   const handleClick = () => {
-    // console.log(
-    //   topTracks?.map(
-    //     ({name, artists}) =>
-    //       `${name} by ${artists.map(artist => artist.name).join(', ')}`
-    //   )
-    // );
+    getTopTracks(token).then((tracks) => setTracks(tracks));
   };
 
   return (
     <div className="App">
       <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content text-center">
-          <div className="max-w-md">
+        <div className="hero-content text-center w-full">
+          <div>
             <h1 className="text-5xl font-bold">Your Spotify Top Tracks</h1>
             <p className="py-6">
               Generate your top 10 Spotify tracks for the last 30 days
             </p>
             {token ? (
-              <button className="btn btn-accent" onClick={handleClick}>
-                Generate tracks
+              <button className="btn btn-primary" onClick={handleClick}>
+                Generate top tracks
               </button>
             ) : (
               <button className="btn btn-success" onClick={logIn}>
                 Log in with Spotify
               </button>
+            )}
+            {tracks && (
+              <div className="pt-10 prose text-center">
+                <h2>Your top tracks for the last 30 days:</h2>
+                {tracks?.map(({ name, artists }) => (
+                  <h3>
+                    {name} by {artists.map((artist) => artist.name).join(", ")}
+                  </h3>
+                ))}
+              </div>
             )}
           </div>
         </div>
--- src/spotifyAPI.js
+++ src/spotifyAPI.js
@@ -64,21 +64,24 @@ export async function getAccessToken(clientId, code) {
   return access_token;
 }
 
-async function fetchWebApi(endpoint, method, body) {
-  const token = await getAccessToken(clientId, code);
+async function fetchWebApi(endpoint, method, token, body) {
   const res = await fetch(`https://api.spotify.com/${endpoint}`, {
     headers: {
       Authorization: `Bearer ${token}`,
     },
     method,
-    body:JSON.stringify(body)
+    body: JSON.stringify(body),
   });
   return await res.json();
 }
 
-export async function getTopTracks(){
+export async function getTopTracks(token) {
   // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
-  return (await fetchWebApi(
-    'v1/me/top/tracks?time_range=short_term&limit=10', 'GET'
-  )).items;
+  return (
+    await fetchWebApi(
+      "v1/me/top/tracks?time_range=short_term&limit=10",
+      "GET",
+      token
+    )
+  ).items;
 }
