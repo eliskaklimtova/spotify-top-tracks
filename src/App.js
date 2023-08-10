c74f04e8491a5212abf6adbf6e25632a0823a0d9
eliskaklimtova <eli.klimtova@gmail.com>
2023-08-10 11:32:29 +0200

    Update rendering logic

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ function App() {
     getTopTracks(token).then((tracks) => setTracks(tracks));
   };
 
+  if (code && !token) {
+    return null;
+  }
+
   return (
     <div className="App">
       <div className="hero min-h-screen bg-base-200">
@@ -32,25 +36,30 @@ function App() {
             <p className="py-6">
               Generate your top 10 Spotify tracks for the last 30 days
             </p>
-            {token ? (
-              <button className="btn btn-primary" onClick={handleClick}>
-                Generate top tracks
-              </button>
-            ) : (
-              <button className="btn btn-success" onClick={logIn}>
-                Log in with Spotify
-              </button>
-            )}
-            {tracks && (
+            {!tracks &&
+              (token ? (
+                <button className="btn btn-primary" onClick={handleClick}>
+                  Generate top tracks
+                </button>
+              ) : (
+                <button className="btn btn-success" onClick={logIn}>
+                  Log in with Spotify
+                </button>
+              ))}
+
+            {tracks && 
               <div className="pt-10 prose text-center">
                 <h2>Your top tracks for the last 30 days:</h2>
-                {tracks?.map(({ name, artists }) => (
-                  <h3>
-                    {name} by {artists.map((artist) => artist.name).join(", ")}
-                  </h3>
-                ))}
+                <ol className="flex flex-col items-center text-lg font-medium">
+                  {tracks?.map(({ name, artists }) => (
+                    <li className="text-left">
+                      {name} by {" "}
+                      {artists.map((artist) => artist.name).join(", ")}
+                    </li>
+                  ))}
+                </ol>
               </div>
-            )}
+            }
           </div>
         </div>
       </div>
